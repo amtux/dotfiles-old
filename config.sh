3c0a8b13fcf567560a40c22b782adc874a12b53d
Amandeep Uppal <uppal@ca.ibm.com>
2016-06-11 00:56:36 -0400

    edit stuff

---
+++ config.sh
@@ -0,0 +1,23 @@
+# configure stuff here
+#  - - - - - - - - - #
+
+# configs will be installed for this user.
+# user will be created if doesn't exist
+target_user="aman"
+
+# upgrade packages
+upgrade_packages="true"
+
+# install zsh
+install_ohmyzsh="true"
+
+# install neovim
+install_neovim="true"
+install_neovim_config="true"
+alias_neovim_to_vim="true"
+
+# install extra utils
+install_utils="true"
+
+# install docker
+install_docker="true"
